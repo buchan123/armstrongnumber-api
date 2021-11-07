7ad213d0adf1f74c3b0371e95de0dbebedb8c9b3
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2021-11-07 13:42:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,9 @@ app.use("*", (req, res) => {
     },
   });
 });
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
-app.listen(() => {
+app.listen(port,host,() => {
   console.log("Server Started.");
 });
